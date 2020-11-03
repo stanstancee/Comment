7b4b96a7a308ab48b999b587695bc84703fa3ff4
stanstancee <stanstancee@gmail.com>
2020-11-03 22:09:28 +0100

    change phone to string

--- models/contactModel.js
+++ models/contactModel.js
@@ -5,7 +5,7 @@ const {Schema} = mongoose;
 const contactModel = new Schema({
   name:{type:String} ,
   email: {type:String},
-  phone: {type:Number},
+  phone: {type:String},
   message: {type:String}
 });
 
