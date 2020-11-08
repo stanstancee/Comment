4460e12d2ac374ec57976d41224d17eb1c7bac77
stanstancee <stanstancee@gmail.com>
2020-11-08 03:40:52 +0100

    add features

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -35,6 +35,72 @@ if(!req.body.link){
         
     });
 });
+projectRouter.use('/project/:projectId', (req, res, next) => {
+   Project.findById(req.params.projectId, (err, foundProject) => {
+        if (err) {
+            return res.send(`You have an error ${err}`);
+        }
+        if (foundProject) {
+            req.foundProject = foundProject;
+            return next();
+        }
+        return res.sendStatus(404);
+    })
+});
+projectRouter.route('/project/:projectId')
+    .get((req, res) =>{ 
+        const returnProject = req.foundProject.toJSON();
+   
+       return  res.json(returnProject)
+
+    }
+    )
+    .put((req, res) => {
+        const {
+            foundProject
+        } = req;
+       foundProject.link=req.body.link
+        foundProject.title = req.body.title;
+        foundProject.content = req.body.content;
+        req.foundProject.save((err) => {
+            if (err) {
+                return res.sendStatus(500).send(err)
+            } else {
+                return res.json(req.foundProject)
+            }
+        })
+        return res.status(201).json(foundProject);
+    })
+
+    .patch((req, res) => {
+        const {
+            foundProject
+        } = req;
+        if (req.body._id) {
+            delete req.body._id;
+        }
+        Object.entries(req.body).forEach((item) => {
+            const key = item[0]
+            const value = item[1];
+            foundProject[key] = value;
+        });
+        req.foundProject.save((err) => {
+            if (err) {
+                return res.sendStatus(500).send(err)
+            } else {
+                return res.json(req.foundProject)
+            }
+        })
+    })
+    .delete((req, res) => {
+        req.foundProject.remove(err => {
+            if (err) {
+                return res.send(err)
+            } else {
+                return res.sendStatus(204);
+            }
+        });
+    });
 return projectRouter
 }
 module.exports = projectRouter
